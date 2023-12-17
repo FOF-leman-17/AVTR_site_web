dce8cf773d44b3b41105f17916ac668cf1bddb37
FOF-leman-17 <fofleman17@gmail.com>
2023-12-17 02:33:26 +0000

    site de presentation de la mercedes AVTR *

---
+++ test.js
@@ -0,0 +1,23 @@
+// // var Marque;
+// var Marque = "toyota";
+// // var Modele;
+// var Modele = "riyas";
+// var porte = 3;
+// var annee = 2002;
+// var age = 2023-annee ;
+
+
+
+// console.log("MARQUE " + Marque);
+// console.log("Modele " + Modele);
+// console.log("Nombre de porte " + porte);
+// console.log("la voiture date de %d, elle a donc %d ans",annee,age ) ;
+// // console.log("le score est de " + score + " points" );
+
+var Note;
+Note = 12;
+Note += 14;
+Note += 16;
+
+console.log("la moyenne de Marce est", Note/3 );
+
